685a5e7090f203ded10c3df743fe11034664412e
OskarGU <oskargstfssn@gmail.com>
2025-07-05 22:06:26 +0200

    updated docs/make.jl file

--- docs/make.jl
+++ docs/make.jl
@@ -5,11 +5,12 @@ DocMeta.setdocmeta!(BOOP, :DocTestSetup, :(using BOOP); recursive=true)
 
 makedocs(;
     modules=[BOOP],
-    authors="Oskar Gustafsson",
+    authors="Oskar Gustafsson < oskar.gstfssn@gmail.com >",
+    repo="https://github.com/OskarGU/BOOP.jl/blob/{commit}{path}#{line}", # Added this line
     sitename="BOOP.jl",
     format=Documenter.HTML(;
         canonical="https://OskarGU.github.io/BOOP.jl",
-        edit_link="master",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
@@ -19,5 +20,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/OskarGU/BOOP.jl",
-    devbranch="master",
+    devbranch="main",
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -6,6 +6,8 @@ CurrentModule = BOOP
 
 Documentation for [BOOP](https://github.com/OskarGU/BOOP.jl).
 
+This is a get starting message to see that every thing works and we can add stuff.
+
 ```@index
 ```
 
