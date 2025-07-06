59da4ca2a6443d184455d46760d2c4a76eedff61
OskarGU <oskargstfssn@gmail.com>
2025-07-06 13:30:15 +0200

    addad dependencies and files for acquisition functions

--- src/BOOP.jl
+++ src/BOOP.jl
@@ -1,5 +1,8 @@
 module BOOP
 
+# Using other packages
+using LinearAlgebra, Distributions, Random, Statistics, Optim, Plots, SpecialFunctions
+
 # Write your package code here.
 include("FirstFunc.jl")
 export first_func
